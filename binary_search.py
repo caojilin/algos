5219ebda69010f641d1fc47e3e67ecf65e7a51fa
caojilin <caojilin@berkeley.edu>
2022-08-26 14:53:34 -0700

    Create binary_search.py

---
+++ binary_search.py
@@ -0,0 +1,20 @@
+arr = list(range(1,10))
+
+def binary(arr, x):
+    print('search for', x)
+    l = 0
+    r = len(arr)-1
+    while l < r:
+        mid = l + (r-l)//2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] > x:
+            r = mid
+        else:
+            l = mid + 1
+    return l
+
+print(arr)
+print(binary(arr, 5))
+print(binary(arr, 5.5))
+print(binary(arr, 4.5))
